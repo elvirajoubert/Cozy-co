e58e0b5f182523cdf2b2bf8cad21fb94212be6e8
Elvira Joubert <reagent01@gmail.com>
2019-05-01 20:08:09 -0500

    added Services, Migration

---
+++ Cozy-co/CozyCo.Data/Interfaces/IPropertyRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozyCo.Data.Interfaces
+{
+    interface IPropertyRepository
+    {
+    }
+}
---
+++ Cozy-co/CozyCo.Service/Services/PropertyService.cs
@@ -0,0 +1,25 @@
+﻿namespace CozyCo.Service.Services
+{
+    public interface IPropertyService
+    {
+        //Create Read Update Delete or RCUD
+
+
+        Property GetById(int id);
+        //ICollection<Property> GetByLandlordId(int landlordId);
+
+        //Create
+        Property Create(Property newProperty);
+
+        //Update
+        Property Update(Property updatedProperty);
+
+        //Delete
+        bool Delete(int id);
+    }
+
+    public class PropertyService
+    {
+
+    }
+}
